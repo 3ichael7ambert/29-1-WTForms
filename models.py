1bf5bcef569e4a20114c2334425bfd396b745ce0
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-06-07 20:42:14 -0600

    commit

---
+++ models.py
@@ -0,0 +1,36 @@
+"""Models for adopt app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+GENERIC_IMAGE = "https://www.hancockregionalhospital.org/wp-content/uploads/Blog_Photos/Dog-paw-print-with-transparent-background_164879_d200.png"
+
+db = SQLAlchemy()
+
+
+class Pet(db.Model):
+    """Adoptable pet."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable=False, default=True)
+
+    def image_url(self):
+        """Return image for pet -- bespoke or generic."""
+
+        return self.photo_url or GENERIC_IMAGE
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
---
+++ readme.md
@@ -0,0 +1,21 @@
+# WTForms
+----------------------------------------------------------------
+Be sure to seed the Database first
+```bash
+psql > seed_adopt.sql
+```
+
+```bash
+pip install Flask Flask-SQLAlchemy Flask-WTF
+pip install flask
+pip install flask_sqlalchemy
+pip install flask_wtf
+pip install flask_debugtoolbar
+pip install psycopg2
+
+
+
+
+pip install -r requirements.txt
+
+```
